3c1e3dbf623b1922ae1acf1320b6b10248eb6587
PranjaliRawke05 <pranjaliprawke@gmail.com>
2021-03-02 15:52:37 -0800

    Calculate Parttime

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation"
-
 att=$((RANDOM%2))
 if [ $att -eq 0 ]
 then
@@ -9,3 +8,19 @@ then
 else
 	echo "Employee id Present"
 fi
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
+case $empCheck in
+        $isFullTime)
+                empHrs=8;;
+        $isPartTime)
+                empHrs=4;;
+        *)
+                empHrs=0;;
+esac
+
+salary=$(($empHrs*$empRatePerHr));
+echo "Salary ::"$salary
