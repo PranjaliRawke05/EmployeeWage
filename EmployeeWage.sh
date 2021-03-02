f012972b1a34e06b0bb75439605925a9fd069293
PranjaliRawke05 <56476730+PranjaliRawke05@users.noreply.github.com>
2021-03-02 02:59:26 -0800

    Update EmployeeWage.sh

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -12,17 +12,21 @@ fi
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
-        $isFullTime)
-                empHrs=8;;
-        $isPartTime)
-                empHrs=4;;
-        *)
-                empHrs=0;;
-esac
-
+totalSalary=0;
+numWorkingDays=20;
+for (( day=1; day<=$numWorkingDays; day++))
+do
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+        	$isFullTime)
+                	empHrs=8;;
+        	$isPartTime)
+                	empHrs=4;;
+        	*)
+                	empHrs=0;;
+	esac
 salary=$(($empHrs*$empRatePerHr));
-wageMonth=$(($salary*20))
+totalSalary=$(($totalSalary+$salary))
+done
 echo "Salary ::"$salary
-echo "Wages for a Month:"$wageMonth
+echo "Wages for a Month:"$totalSalary
